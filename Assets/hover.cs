6ad151749f83665c865d785a6ebfb119c4e19e2e
omar.soa <44102095+soablackwhite@users.noreply.github.com>
2021-05-13 22:24:34 +0400

    added to be continued

--- Assets/hover.cs
+++ Assets/hover.cs
@@ -6,17 +6,19 @@ public class hover : MonoBehaviour
 {
     Vector3 increment;
     public float height = 0;
+    public float yOrig = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 _vec = transform.localPosition;
+        yOrig = _vec.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 _vec = transform.localPosition;
-        _vec.y = height * Mathf.Cos(2 * Time.time);
+        _vec.y = yOrig + height * Mathf.Cos(2 * Time.time);
         transform.localPosition = _vec;
         /*increment = new Vector3(0, height*Mathf.Cos(2*Time.time), 0);
         transform.localPosition = increment;*/
